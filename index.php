7fa4100714b441747f7bbffbbd4f55fb33f78929
Yuriv Anton <yurivanton@gmail.com>
2022-10-10 22:25:27 +0200

    index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>oop</title>
+    </head>
+    <body>
+    Menu
+    <ol>
+        <li><a href="oopMVC/properties.php">Properies</a></li>
+        <li><a href="oopMVC/methods.php">
+    Methods</a></li>
+        <li><a href="oopMVC/properties_and_this.php">Properties and this</a></li>
+        <li><a href="oopMVC/constructor.php">Constructor</a></li>
+        <li><a href="oopMVC/getters_and_setters.php">Getters and setters</a></li>
+        <li><a href="oopMVC/read_only_properties.php">Read only properties</a></li>
+        <li><a href="oopMVC/objects_in_array.php">Objects in array</a></li>
+        <li><a href="oopMVC/constructor_initial_properties_values.php">Constructor initial properties values</a></li>
+        <li><a href="oopMVC/declaring_initial_prolerties_values.php">Declaring initial properties values</a></li>
+    </ol>
+    </body>
+</html>
