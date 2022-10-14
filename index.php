7b97a8f53909858237b0c94586a5926d99e323f2
Anton <anton@sin.sk>
2022-10-14 13:37:16 +0200

    chenge index.php

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <title>oop</title>
-    </head>
-    <body>
+
+<head>
+    <meta charset="utf-8">
+    <title>oop</title>
+</head>
+
+<body>
     Menu
     <ol>
         <li><a href="oopMVC/properties.php">Properies</a></li>
-        <li><a href="oopMVC/methods.php">
-    Methods</a></li>
+        <li><a href="oopMVC/methods.php">Methods</a></li>
         <li><a href="oopMVC/properties_and_this.php">Properties and this</a></li>
         <li><a href="oopMVC/constructor.php">Constructor</a></li>
         <li><a href="oopMVC/getters_and_setters.php">Getters and setters</a></li>
@@ -17,8 +18,9 @@
         <li><a href="oopMVC/objects_in_array.php">Objects in array</a></li>
         <li><a href="oopMVC/constructor_initial_properties_values.php">Constructor initial properties values</a></li>
         <li><a href="oopMVC/declaring_initial_prolerties_values.php">Declaring initial properties values</a></li>
-        <li><a href="oopMVC/variable_properties_names.php">Variable properties names</a></li>
-        <li><a href="oopMVC/class_as_methods_set.php">Class as methods set</a></li>
+        <li><a href="variable_properties_names.php">Variable properties names</a></li>
+        <li><a href="class_as_methods_set.php">Class as methods set</a></li>
     </ol>
-    </body>
+</body>
+
 </html>
